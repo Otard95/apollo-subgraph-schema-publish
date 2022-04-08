13ee1ab710245db5601b3401ab36a82e9ea0d597
StianM <stian.myklebostad@schibsted.com>
2022-04-08 10:09:04 +0200

    Fix schema error - empty Query definition

--- server-business/src/schema/index.ts
+++ server-business/src/schema/index.ts
@@ -5,7 +5,7 @@ import { Resolvers } from '@/generated/graphql'
 // We want to be able to use `extend type` on Query and Mutation in our schemas.
 // To be able to do this consistently, we need to initially define them first.
 const baseTypeDef = gql`
-  type Query {}
+  type Query
 `
 
 export const typeDefs = [baseTypeDef, contract.typeDef]
